f9122a359b6ce2d9151ecae4d20b1bacfd204e8c
Adluri Raviteja <adluriraviteja182@gmail.com>
2026-05-02 07:34:53 +0530

    Update meetService.js

--- services/meetService.js
+++ services/meetService.js
@@ -1,7 +1,9 @@
-const { v4: uuidv4 } = require("uuid");
-
 function generateMeetLink() {
-  const roomId = uuidv4().replace(/-/g, "").substring(0, 10);
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let roomId = '';
+  for (let i = 0; i < 10; i++) {
+    roomId += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
   const formatted = roomId.match(/.{1,3}/g).join("-");
   return `https://meet.google.com/${formatted}`;
 }
