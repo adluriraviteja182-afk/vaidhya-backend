7f1415c1743769d152cbf97c8a74808e7eb36b45
Adluri Raviteja <adluriraviteja182@gmail.com>
2026-05-03 10:16:27 +0530

    Add upload routes to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const appointmentRoutes = require("./routes/appointments");
 const authRoutes        = require("./routes/auth");
 const adminRoutes       = require("./routes/admin");
 const adminAuthRoutes   = require("./routes/adminAuth");
+const uploadRoutes      = require("./routes/upload");
 
 const app = express();
 
@@ -17,10 +18,11 @@ app.get("/", (req, res) => {
 
 app.get("/debug", (req, res) => {
   res.json({
-    ADMIN_USERNAME_set: !!process.env.ADMIN_USERNAME,
-    ADMIN_PASSWORD_set: !!process.env.ADMIN_PASSWORD,
-    FRONTEND_URL: process.env.FRONTEND_URL || "not set",
-    DB_HOST_set: !!process.env.DB_HOST,
+    ADMIN_USERNAME_set:      !!process.env.ADMIN_USERNAME,
+    ADMIN_PASSWORD_set:      !!process.env.ADMIN_PASSWORD,
+    CLOUDINARY_set:          !!process.env.CLOUDINARY_CLOUD_NAME,
+    FRONTEND_URL:            process.env.FRONTEND_URL || "not set",
+    DB_HOST_set:             !!process.env.DB_HOST,
   });
 });
 
@@ -28,5 +30,6 @@ app.use("/api/appointments", appointmentRoutes);
 app.use("/api/auth",         authRoutes);
 app.use("/api/admin",        adminRoutes);
 app.use("/api/admin-auth",   adminAuthRoutes);
+app.use("/api/upload",       uploadRoutes);
 
 module.exports = app;
