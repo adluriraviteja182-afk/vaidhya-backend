ee4993f68f0cf99a2fb643ebbe14ffcbfe027b6c
Adluri Raviteja <adluriraviteja182@gmail.com>
2026-05-04 17:12:34 +0530

    Delete routes/admin.js

--- routes/admin.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router  = express.Router();
-const { getStats, getPatients } = require("../controllers/adminController");
-
-router.get("/stats",    getStats);
-router.get("/patients", getPatients);
-
-module.exports = router;
